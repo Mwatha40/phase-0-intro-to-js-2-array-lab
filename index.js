680e0981b9332a28ec41a1397e7ffabd6637a058
Allan <doshbank884@gmail.com>
2024-04-05 08:11:35 +0300

    phase-0-intro-to-js-2-array-lab complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+// index.js
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const newArray=[...cats,name]
+    return newArray;
+}
+
+function prependCat(name) {
+    const newArray =[name, ...cats]
+    return newArray;
+}
+
+function removeLastCat( ){
+    const newArray = cats.slice(0,-1)
+    return newArray;
+}
+const newCatsArrays = removeLastCat()
+function removeFirstCat() {
+    const newCat =cats.slice(1)
+    return newCat;
+}
+const newCat = removeFirstCat();
